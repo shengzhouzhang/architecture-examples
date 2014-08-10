ce61788b8f3b021e327fa09e64d9dce9bd462529
Shengzhou Zhang <steven.shengzhou@gmail.com>
2014-08-10 21:26:26 +1000

    remove events

--- redis/event/events.js
+++
@@ -1,8 +0,0 @@
-define(function(require, exports) {
-  'use strict';
-
-  exports.ADD_SUBSCRIBER = 'ADD_SUBSCRIBER';
-  exports.DELETE_SUBSCRIBER = 'DELETE_SUBSCRIBER';
-  exports.GET_SUBSCRIBER = 'GET_SUBSCRIBER';
-  exports.GET_ALL_SUBSCRIBER = 'GET_ALL_SUBSCRIBER';
-});
